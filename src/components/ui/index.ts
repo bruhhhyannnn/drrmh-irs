e54d7fdc8dbd753dd4b97484799544b7d2e3f17f
Bryan Mangapit <bryanjesusmangapit477@gmail.com>
2026-06-08 09:32:26 +0800

    feat(map): Install mapcn library for mapping feature

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,6 +4,15 @@ export { ConfirmDialog } from './confirm-dialog';
 export { DataTable } from './data-table';
 export { Dropdown, DropdownItem } from './dropdown';
 export { Input, Label, Select, Textarea } from './form';
+export {
+  Map,
+  MapControls,
+  MapMarker,
+  MarkerContent,
+  MarkerPopup,
+  MarkerTooltip,
+  useMap,
+} from './map';
 export { Modal } from './modal';
 export { Pagination } from './pagination';
 export { PageError, Spinner } from './spinner';
