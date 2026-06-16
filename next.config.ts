79950a906b3bebbce7851f4aef3ce2de7d3cf0c4
Bryan Mangapit <100248773+bruhhhyannnn@users.noreply.github.com>
2026-06-16 16:52:32 +0800

    Remove allowedDevOrigins from next.config.ts
    Remove allowedDevOrigins configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,6 @@ const nextConfig: NextConfig = {
   turbopack: {},
 };
 
-module.exports = {
-  allowedDevOrigins: ['10.49.218.251'],
-};
-
 export default withPWA({
   dest: 'public',
   cacheOnFrontEndNav: true,
